fc06095cf0fa9d3356dc261360151ceb11847e60
zonway <3126599489@qq.com>
2018-07-11 22:36:32 +0800

    update

--- src/main/java/com/zn/algorithm/sort/HeapSort.java
+++ src/main/java/com/zn/algorithm/sort/HeapSort.java
@@ -3,13 +3,63 @@ package com.zn.algorithm.sort;
 /**
  * 堆排序
  * 对选择排序的优化
+ * <p>
+ * 堆的某个节点的值总是大于等于子节点的值，并且堆是一颗完全二叉树
+ * 堆可以用数组来表示，因为堆是完全二叉树，而完全二叉树很容易就存储在数组中。
+ * 位置 k 的节点的父节点位置为 k/2，而它的两个子节点的位置分别为 2k 和 2k+1。这里不使用数组索引为 0 的位置，
+ * 是为了更清晰地描述节点的位置关系。
+ * <p>
  * Created by zhoun on 2018/7/10.
  **/
 public class HeapSort {
 
-    public void sort() {
-
+    public void heapSort(int[] a) {
+        int len = a.length;
+        //循环建堆
+        for (int i = 0; i < len - 1; i++) {
+            //建堆
+            buildMaxHeap(a, len - 1 - i);
+            //交换堆顶和最后一个元素
+            swap(a, 0, len - 1 - i);
+        }
     }
 
+    //交换方法
+    private void swap(int[] data, int i, int j) {
+        int tmp = data[i];
+        data[i] = data[j];
+        data[j] = tmp;
+    }
+
+    //对data数组从0到lastIndex建大顶堆
+    private void buildMaxHeap(int[] data, int lastIndex) {
+        //从lastIndex处节点（最后一个节点）的父节点开始
+        for (int i = (lastIndex - 1) / 2; i >= 0; i--) {
+            //k保存正在判断的节点
+            int k = i;
+            //如果当前k节点的子节点存在
+            while (k * 2 + 1 <= lastIndex) {
+                //k节点的左子节点的索引
+                int biggerIndex = 2 * k + 1;
+                //如果biggerIndex小于lastIndex，即biggerIndex+1代表的k节点的右子节点存在
+                if (biggerIndex < lastIndex) {
+                    //若果右子节点的值较大
+                    if (data[biggerIndex] < data[biggerIndex + 1]) {
+                        //biggerIndex总是记录较大子节点的索引
+                        biggerIndex++;
+                    }
+                }
+                //如果k节点的值小于其较大的子节点的值
+                if (data[k] < data[biggerIndex]) {
+                    //交换他们
+                    swap(data, k, biggerIndex);
+                    //将biggerIndex赋予k，开始while循环的下一次循环，重新保证k节点的值大于其左右子节点的值
+                    k = biggerIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 
 }
