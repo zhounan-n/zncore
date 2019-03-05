81dd3da00f6d7e8dfdc319ff04f793c1b124f522
zonway <3126599489@qq.com>
2019-03-05 18:07:34 +0800

    简单本地缓存实现

--- src/main/java/com/zn/multithread/ProducerConsumer2.java
+++ src/main/java/com/zn/multithread/ProducerConsumer2.java
@@ -1,5 +1,7 @@
 package com.zn.multithread;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * wait notify实现
  * Created by zhoun on 2019/3/3.
@@ -58,6 +60,7 @@ public class ProducerConsumer2 {
                     count--;
                     System.out.println(Thread.currentThread() + "消费者消费,目前共有" + count);
                     LOCK.notifyAll();
+                    //只有当 notify/notifyAll() 被执行时候，才会唤醒一个或多个正处于等待状态的线程，然后继续往下执行，
                 }
             }
         }
